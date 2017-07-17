f20d5bfec4f3652cbf7a15ff7c88d659d2bf213f
zhoushunli <649836711@qq.com>
2017-07-17 22:22:39 +0800

    播放页面更改2

--- src/main/java/com/zhousl/musicplayer/util/TimeUtil.java
+++ src/main/java/com/zhousl/musicplayer/util/TimeUtil.java
@@ -8,27 +8,35 @@ public class TimeUtil {
 
     public static String getFormattedTimeStr(long time) {
         if (time <= 0)
-            return "";
-        if (time > 1000) {
-            long seconds = time / 1000;
-            if (seconds >= 60) {
-                long minutes = seconds / 60;
-                if (minutes >= 60) {
-                    long hour = minutes / 60;
-                    return hour + ":" + (minutes - hour * 60) + ":" + (time - hour * 3600 - minutes * 60);
-                } else {
-                    return seconds + ":" + (time - minutes * 60);
-                }
-            } else {
-                return "00:" + seconds;
-            }
+            return "00:00";
+        long seconds = getSeconds(time);
+        if (seconds < 60) {
+            return "00:" + seconds;
         } else {
-            return "";
+            long minutes = getMinutes(time);
+            if (minutes < 60) {
+                return (minutes >= 10 ? minutes : ("0" + minutes)) + ":" + (seconds - minutes * 60);
+            } else {
+                long hour = getHour(time);
+                return (hour >= 10 ? hour : ("0:" + hour)) + ":"
+                        + ((minutes - hour * 60) >= 10 ? (minutes - hour * 60) : ("0:" + (minutes - hour * 60))) + ":"
+                        + ((seconds - minutes * 60 - hour * 3600) >= 10 ? (seconds - minutes * 60 - hour * 3600) : ("0:" + (seconds - minutes * 60 - hour * 3600)));
+            }
         }
     }
 
     private static long getSeconds(long time) {
         return time / 1000;
+    }
+
+    private static long getMinutes(long time) {
+        long seconds = getSeconds(time);
+        return seconds / 60;
+    }
+
+    private static long getHour(long time) {
+        long minutes = getMinutes(time);
+        return minutes / 60;
     }
 
 }
--- src/main/java/com/zhousl/musicplayer/view/PlayingActivity.java
+++ src/main/java/com/zhousl/musicplayer/view/PlayingActivity.java
@@ -1,30 +1,45 @@
 package com.zhousl.musicplayer.view;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Shader;
 import android.os.Bundle;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.AppCompatSeekBar;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
+import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
 import com.zhousl.musicplayer.BaseActivity;
 import com.zhousl.musicplayer.Music;
 import com.zhousl.musicplayer.MusicPlayer;
 import com.zhousl.musicplayer.R;
 import com.zhousl.musicplayer.interf.Player;
 import com.zhousl.musicplayer.util.TimeUtil;
+import com.zhousl.musicplayer.util.UIUtil;
+
+import jp.wasabeef.glide.transformations.BlurTransformation;
+import jp.wasabeef.glide.transformations.CropCircleTransformation;
 
 /**
  * Created by shunli on 2017/5/1.
  */
 
-public class PlayingActivity extends BaseActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
+public class PlayingActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
 
     private ImageView album;
     private Toolbar toolbar;
@@ -40,6 +55,8 @@ public class PlayingActivity extends BaseActivity implements View.OnClickListene
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_play);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         init();
         attachViewState();
     }
@@ -53,9 +70,11 @@ public class PlayingActivity extends BaseActivity implements View.OnClickListene
         }
         toolbar.setTitle(player.getMusic().getName());
         toolbar.setSubtitle(player.getMusic().getArtist());
-        if (player.getMusic().getState()== Music.MusicState.STATE_PLAYING){
+        ((RelativeLayout.LayoutParams) toolbar.getLayoutParams()).topMargin= UIUtil.getStatusBarHeight(this);
+        toolbar.requestLayout();
+        if (player.getMusic().getState() == Music.MusicState.STATE_PLAYING) {
             playPause.setImageResource(R.mipmap.m_play);
-        }else {
+        } else {
             playPause.setImageResource(R.mipmap.m_pause);
         }
         artist.setText(player.getMusic().getArtist());
@@ -70,6 +89,13 @@ public class PlayingActivity extends BaseActivity implements View.OnClickListene
         album = (ImageView) findViewById(R.id.album);
         artist = (TextView) findViewById(R.id.artist);
         lrc = (TextView) findViewById(R.id.lrc);
+        AppCompatImageView playBackground = (AppCompatImageView) findViewById(R.id.play_background);
+        Glide.with(this)
+                .load(R.drawable.play_bg)
+                .centerCrop()
+                .crossFade(500)
+                .bitmapTransform(new BlurTransformation(this,30))
+                .into(playBackground);
         findViewById(R.id.play_state).setOnClickListener(this);
         findViewById(R.id.play_previous).setOnClickListener(this);
         playPause = (AppCompatImageView) findViewById(R.id.play_pause);
@@ -80,14 +106,13 @@ public class PlayingActivity extends BaseActivity implements View.OnClickListene
         progress = (AppCompatSeekBar) findViewById(R.id.play_progress);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
-        RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(), BitmapFactory.decodeResource(getResources(), R.drawable.play_bg));
-        roundedBitmapDrawable.setCircular(true);
-        album.setImageDrawable(roundedBitmapDrawable);
-    }
-
-    @Override
-    protected int getRes() {
-        return R.layout.act_play;
+//        RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(), BitmapFactory.decodeResource(getResources(), R.drawable.play_bg));
+//        roundedBitmapDrawable.setCircular(true);
+//        album.setImageDrawable(roundedBitmapDrawable);
+        Glide.with(this)
+                .load(R.drawable.play_bg)
+                .bitmapTransform(new CropCircleTransformation(this))
+                .into(album);
     }
 
     @Override
